73e794ac72cf59f813a8a2164d0236fc95ebc4d9
yazdog8 <joe@pauilsenweb.com>
2020-12-30 10:41:19 -0800

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,8 @@ const dependencies = require('../package.json').dependencies;
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
