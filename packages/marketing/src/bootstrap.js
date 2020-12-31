791bd4fb7307d2489ae1d4be8cc6267bf0fb145a
yazdog8 <joe@pauilsenweb.com>
2020-12-31 10:59:40 -0800

    Keep history in sync

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, {onNavigate}) => {
+  const history = createMemoryHistory();
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({pathname: nextPathname}) {
+      const {pathname} = history.location;
+
+      if (pathname !== nextPathname) history.push(nextPathname);
+    }
+  }
 };
 
 // If we are in development and in isolation,
@@ -13,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
