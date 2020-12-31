4881df8b6234e59de6443132519c47c96a177761
yazdog8 <joe@pauilsenweb.com>
2020-12-31 11:09:15 -0800

    Update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
-import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
-import Header from "./components/Header";
+import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co'
-})
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-      <BrowserRouter>
-        <StylesProvider generateClassName={generateClassName}>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
-        </StylesProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,7 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const dependencies = require('../package.json').dependencies;
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -19,7 +19,7 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
-      shared: dependencies,
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
